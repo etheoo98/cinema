3e553a6e8d7ffd8f5bf6d2793620ddffcd4a46d4
etheoo <etheoo@protonmail.com>
2023-04-14 14:31:08 +0200

    Added User Rating for Movies again

--- views/title/index.php
+++ views/title/index.php
@@ -27,9 +27,11 @@
     </header>
     <div class="movie-body">
         <div class="container">
+
             <div class="movie-metadata">
                 <p class="movie-description"><?php echo $this->titleData["description"]; ?></p>
             </div>
+
             <div class="movie-metadata">
                 <h3>Starring:</h3>
                 <ul class="movie-starring-list">
@@ -38,18 +40,36 @@
                     <?php endforeach; ?>
                 </ul>
             </div>
+
+            <div class="movie-metadata">
+                <h3>User Rating:</h3>
+                <?php if (!empty($this->ratingData) && $this->ratingData["avg_rating"] != 0) { ?>
+                    <div class="rating-container">
+                        <span><?php echo $this->ratingData["avg_rating"]; ?> / 5.0</span>
+                        <div class="svg-container">
+                            <svg id="star-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                        </div>
+                    </div>
+                <?php } else { ?>
+                    <span>Not Yet Rated</span>
+                <?php } ?>
+            </div>
+
             <div class="movie-metadata">
                 <h3>Language:</h3>
                 <p class="movie-description"><?php echo $this->titleData["language"]; ?></p>
             </div>
+
             <div class="movie-metadata">
                 <h3>Subtitles:</h3>
                 <p class="movie-description">Yes/No</p>
             </div>
+
             <div class="movie-metadata">
                 <h3>Premiere:</h3>
                 <p class="movie-description"><?php echo $this->titleData["premiere"]; ?></p>
             </div>
+
         </div>
     </div>
 </main>
