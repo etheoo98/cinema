bd4340e742095cc7c76b202921ee51cfcf826afe
etheoo <etheoo@protonmail.com>
2023-04-14 15:27:56 +0200

    Sessions in Settings to use grid

--- views/settings/index.php
+++ views/settings/index.php
@@ -3,6 +3,7 @@
         <h1 class="page-title">Settings</h1>
     </div>
     <div class="container">
+
         <button type="button" class="collapsible">Profile</button>
         <div class="settings">
             <div class="section">
@@ -10,6 +11,7 @@
                 <h2 class="section-header">Hide Country</h2>
             </div>
         </div>
+
         <button type="button" class="collapsible">Site Preferences</button>
         <div class="settings">
             <div class="section">
@@ -19,6 +21,7 @@
                 <h2 class="section-header">Blacklist genres</h2>
             </div>
         </div>
+
         <button type="button" class="collapsible">Email, username and password</button>
         <div class="settings">
             <div class="section">
@@ -27,7 +30,7 @@
                     <label for="new-email">Enter New Email</label>
                     <input id="new-email" name="new-email" placeholder="<?php echo $_SESSION['email'] ?>" title="Email address that will be used to log in" pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}" required>
                     <div class="form-button">
-                        <button name="change-email">Change Email</button>
+                        <button class="button" name="change-email">Change Email</button>
                     </div>
                 </form>
             </div>
@@ -48,40 +51,35 @@
                 </form>
             </div>
         </div>
+
         <button type="button" class="collapsible">Sessions</button>
         <div class="settings">
             <div class="section">
                 <form class="sessions" method="POST">
-                    <ul>
-                        <li class="list-header list-row">
-                            <div class="list-item">
-                                <input type="checkbox" onClick="toggle(this)">
-                            </div>
-                            <div class="list-item">Signed In</div>
-                            <div class="list-item">Country</div>
-                            <div class="list-item">User Agent</div>
-                        </li>
+                    <div class="grid-container">
+                        <div class="grid-item"><input type="checkbox" onClick="toggle(this)"></div>
+                        <div class="grid-item">Signed In</div>
+                        <div class="grid-item">Country</div>
+                        <div class="grid-item">User Agent</div>
+                        <div class="grid-item"></div>
                         <?php
                         foreach ($this->sessions as $session) {
-                            ?>
-                            <li class="list-row">
-                            <div class="list-item">
-                                <input type="checkbox" name="checkBoxes[]" value="<?php echo $session['phpsessid'] ?>">
-                            </div>
-                            <div class="list-item"><?php echo $session['date'] ?></div>
-                            <div class="list-item"><?php echo $session['country_code'] ?></div>
-                            <div class="list-item"><?php echo $session['user_agent'] ?></div>
-                            <?php if ($session['phpsessid'] == session_id()) { echo "&#8592 This is you"; } ?>
-                            </li>
+                        ?>
+                            <div class="grid-item"><input type="checkbox" name="checkBoxes[]" value="<?php echo $session['phpsessid'] ?>"></div>
+                            <div class="grid-item"><?php echo $session['date'] ?></div>
+                            <div class="grid-item"><?php echo $session['country_code'] ?></div>
+                            <div class="grid-item"><?php echo $session['user_agent'] ?></div>
+                            <div class="grid-item"><?php if ($session['phpsessid'] == session_id()) { echo "&#8592 This is you"; } ?></div>
                         <?php } ?>
-                    </ul>
+                    </div>
                     <p>Beware that terminating the current session will log you out.</p>
                     <div class="form-button">
-                        <button type="submit" name="terminate">Terminate Session(s)</button>
+                        <button class="button" type="submit" name="terminate">Terminate Session(s)</button>
                     </div>
                 </form>
             </div>
         </div>
+
     </div>
 </main>
 <script src="./public/js/settings.js"></script>
