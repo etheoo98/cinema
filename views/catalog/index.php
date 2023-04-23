ae94dd6a7564e004e1d8c627cafc2801fdf27a0c
etheoo <etheoo@protonmail.com>
2023-04-23 02:59:08 +0200

    Improved readability

--- views/catalog/index.php
+++ views/catalog/index.php
@@ -3,6 +3,7 @@
         <h1 class="page-title">Movies</h1>
         <p class="page-desc">These movies are currently being screened</p>
     </div>
+
     <div class="search-bar container">
         <div class="search-order">
             <div class="svg-container">
@@ -19,7 +20,8 @@
             </div>
         </div>
             <form id="search-form">
-                <input type="text" id="search-input" placeholder="Search Title or Genre">
+                <label for="search-input" style="display: none">Search</label>
+                <input type="text" id="search-input" placeholder="Search title or genre">
                 <button class="search-button" type="submit" id="search-button">
                     <div class="svg-container">
                         <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
